e3717efa5de3f394217deda4e294899138323bcb
harisnaseer24 <harisnaseer258@gmail.com>
2024-04-25 10:55:43 +0500

    Operators done

---
+++ 2302B1firstConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+Console.Write("Hello, Haris");// to print on same line
+
+Console.WriteLine("Hello, World");// to print on next line
+
+//Datatypes
+
+byte age = 45;
+short num = 32000;
+
+long num2 = 47745454545l;
+
+
+float percentage = 80.787f;
+double pi = 3.142222432323;
+decimal speedofLight = 3.8000000000000000000000000m;
+
+
+
+char favCharacter = '@';
+string name = "Haris";
+
+
+
+Console.WriteLine("What is your Name..?");
+string name = Console.ReadLine();//string "8"
+
+Console.WriteLine("Welcome "+ name + " , to our console.");
+
+//Operators
+int num1 = 6;
+int num2 = 2;
+
+// Arithmetic operators
+Console.WriteLine(num1 + num2);
+Console.WriteLine(num1 - num2);
+Console.WriteLine(num1 * num2);
+Console.WriteLine(num1 / num2);
+Console.WriteLine(num1 % num2);// remainder
+
+//Assignment Operators
+int number = 3;
+number += 2; // number = number + 2
+Console.WriteLine(number);
+
+number -= 2; // number = number - 2
+Console.WriteLine(number);
+
+number *= 2; // number = number * 2
+Console.WriteLine(number);
+
+number /= 2; // number = number / 2
+Console.WriteLine(number);
+*/
+//Comparison Operators( true / false)
+
+int num1 = 6;
+int num2 = 2;
+
+Console.WriteLine(num1 == num2);//F
+Console.WriteLine(num1 != num2);//T
+Console.WriteLine(num1 > num2);// 6 > 2 //T
+
+Console.WriteLine(num2 < num1); // 2 < 6 //t
+
+Console.WriteLine(num1 <= num2);  //f
+
+Console.WriteLine(num2 >= num1); // 2 >= 6 // false
+
